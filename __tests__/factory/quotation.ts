26adfef81e405f5ee4b8f8d7b28ee85ff9daf687
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-03 17:14:47 -0500

    cria uma factory para quotation

---
+++ __tests__/factory/quotation.ts
@@ -0,0 +1,11 @@
+import { faker } from "@faker-js/faker";
+import { IQuotation } from "src/interfaces/quoatation";
+
+export function quotation(initial?: Partial<IQuotation>) {
+  return {
+    quote: faker.word.words(10),
+    author: faker.person.fullName(),
+    collection: faker.word.words(2),
+    ...initial,
+  }
+}
