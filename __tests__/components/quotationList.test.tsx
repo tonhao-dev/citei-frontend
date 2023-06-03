e8ea6306d0c21c76c5c49eff0718606ddd4c0992
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-03 17:32:46 -0500

    adiciona testes para o componente de quotation list

---
+++ __tests__/components/quotationList.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
+import QuotationList from '@/components/quotationList';
+import { quotation } from '__tests__/factory/quotation';
+
+describe('<QuotationList>', () => {
+  describe('Deve renderizar o componente corretamente quando uma lista de citações for passada via prop', () => {
+    it('Deve exibir o texto das citações quando uma lista de quotation for enviada via prop "quotations"', () => {
+      const quotations = Array.from(Array(faker.number.int({ min: 1, max: 10 }))).map(() => quotation());
+
+      render(<QuotationList quotations={quotations} />);
+
+      quotations.forEach(quotation => expect(screen.getByText(quotation.quote)).toBeInTheDocument());
+    });
+
+    it('Deve exibir o nome dos autores das citações quando uma lista de quotation for passada via prop "quotations"', () => {
+      const quotations = Array.from(Array(faker.number.int({ min: 1, max: 10 }))).map(() => quotation());
+
+      render(<QuotationList quotations={quotations} />);
+
+      quotations.forEach(quotation => expect(screen.getByText(quotation.author)).toBeInTheDocument());
+    });
+
+    it('Deve exibir o nome da coleção das citações quando uma lista de quotation for passada via prop "quotations"', () => {
+      const quotations = Array.from(Array(faker.number.int({ min: 1, max: 10 }))).map(() => quotation({ collection: 'colecao' }));
+
+      render(<QuotationList quotations={quotations} />);
+
+      expect(screen.getAllByText('colecao').length).toBe(quotations.length);
+    });
+  });
+});
