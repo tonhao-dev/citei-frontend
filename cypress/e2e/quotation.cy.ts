b2bfd613c798290111e626b5035caf9f1c19c78d
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-04 19:10:23 -0500

    adiciona testes e2e para a pagina de citacoes

---
+++ cypress/e2e/quotation.cy.ts
@@ -0,0 +1,44 @@
+import { faker } from '@faker-js/faker';
+
+/* eslint-disable */
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+// Cypress E2E Test
+describe('e2e/quotation', () => {
+  describe('Deve carregar a página de citações corretamente', () => {
+    it('Deve exibir o título da página de citações quando ela for carregada', () => {
+      cy.visit('http://localhost:3000/');
+
+      cy.get('h1').contains('Citei');
+    });
+  });
+
+  describe('Deve exibir corretamente o modal de cadastrar citação', () => {
+    it('Deve exibir a citação que foi adicionada pelo usuário através do modal de adicionar citação', () => {
+      const quotation = faker.word.words(10);
+      cy.visit('http://localhost:3000/');
+      cy.get('button').contains('Adicionar citação').click();
+
+      cy.get('textarea').type(quotation);
+      cy.get('select').select(0);
+      cy.get('button').contains('Salvar').click();
+
+      cy.contains(quotation).should('be.visible');
+    });
+
+    it('Deve manter o modal aberto quando o usuário clicar no botão de salvar após deixar o campo de citação em branco', () => {
+      cy.visit('http://localhost:3000/');
+      cy.get('button').contains('Adicionar citação').click();
+
+      cy.get('select').select(0);
+      cy.get('button').contains('Salvar').click();
+
+      cy.get('button').contains('Salvar').should('be.visible');
+    });
+   })
+})
+
+// Prevent TypeScript from reading file as legacy script
+export {}
