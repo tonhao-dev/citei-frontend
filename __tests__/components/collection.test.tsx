a7419ef7422eabad70f9a96ee615ab0f4535427f
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-06 12:09:40 -0500

    injeta as props nos testes de collection por meio da factory

--- __tests__/components/collection.test.tsx
+++ __tests__/components/collection.test.tsx
@@ -2,75 +2,56 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { faker } from '@faker-js/faker';
 import Collection from '../../src/components/collection';
+import { collection } from '../factory/collection';
+import { ImageURL } from '../../src/entities/url';
 
 describe('<Collection>', () => {
   describe('Deve exibir uma coleção corretamente quando as informações passadas forem válidas', () => {
     it('Deve exibir o título da coleção quando ele for passado na prop "title"', () => {
       const title = faker.word.words(2);
-      render(<Collection title={title} />);
+      render(<Collection collection={collection({ title })} />);
 
       expect(screen.getByText(title)).toBeInTheDocument();
     });
 
     it('Deve exibir o subtítulo da coleção quando ele for passado na prop "subtitle"', () => {
       const subtitle = faker.word.words(7);
-      render(<Collection subtitle={subtitle} />);
+      render(<Collection collection={collection({ subtitle })} />);
 
       expect(screen.getByText(subtitle)).toBeInTheDocument();
     });
 
     it('Deve exibir o nome do autor da coleção quando ele for passado na prop "author"', () => {
       const author = faker.person.fullName();
-      render(<Collection author={author} />);
+      render(<Collection collection={collection({ author })} />);
 
       expect(screen.getByText(author)).toBeInTheDocument();
     });
 
     it('Deve exibir o botão de editar coleção quando o componente for renderizado', () => {
-      render(<Collection />);
+      render(<Collection collection={collection()} />);
 
       expect(screen.getByText('Editar')).toBeInTheDocument();
     });
 
     it('Deve exibir o botão de excluir coleção quando o componente for renderizado', () => {
-      render(<Collection />);
+      render(<Collection collection={collection()} />);
 
       expect(screen.getByText('Excluir')).toBeInTheDocument();
     });
 
     it('Deve exibir uma imagem com o atributo src igual ao valor passado na prop "image"', () => {
       const src = faker.image.url();
-      render(<Collection image={src} />);
+      render(<Collection collection={collection({ image: new ImageURL(src) })} />);
 
       expect(screen.getByRole('img').getAttribute('src')).toBe(src);
-    });
-  });
-
-  describe('Deve exibir os valores default quando as informações corretas não forem passadas vias prop', () => {
-    it('Deve exibir o texto "Título da coleção" quando uma coleção for renderizada sem título', () => {
-      render(<Collection />);
-
-      expect(screen.getByText('Título da coleção')).toBeInTheDocument();
-    });
-
-    it('Deve exibir o texto "Subtítulo da coleção" quando uma coleção for renderizada sem subtítulo', () => {
-      render(<Collection />);
-
-      expect(screen.getByText('Subtítulo da coleção')).toBeInTheDocument();
-    });
-
-    it('Deve exibir uma imagem padrão quando um link inválido for passado via prop "image"', () => {
-      const DEFAULT_IMAGE_URL = 'https://i.ibb.co/ZHDSnj4/foo.jpg';
-      render(<Collection image={faker.person.fullName()} />);
-
-      expect(screen.getByRole('img').getAttribute('src')).toBe(DEFAULT_IMAGE_URL);
     });
   });
 
   describe('Deve executar corretamente as ações da coleção quando o usuário clicar nelas', () => {
     it('Deve chamar a função de editar passada via prop "onEdit" quando o usuário clicar no botão Editar', async () => {
       const onEdit = jest.fn();
-      render(<Collection onEdit={onEdit} />);
+      render(<Collection collection={collection()} onEdit={onEdit} />);
 
       await userEvent.click(screen.getByRole('button', { name: 'Editar' }));
 
@@ -79,7 +60,7 @@ describe('<Collection>', () => {
 
     it('Deve chamar a função de excluir passada via prop "onDelete" quando o usuário clicar no botão de Excluir', async () => {
       const onDelete = jest.fn();
-      render(<Collection onDelete={onDelete} />);
+      render(<Collection collection={collection()} onDelete={onDelete} />);
 
       await userEvent.click(screen.getByRole('button', { name: 'Excluir' }));
 
--- __tests__/factory/collection.ts
+++ __tests__/factory/collection.ts
@@ -1,11 +1,13 @@
 import { faker } from "@faker-js/faker";
-import { ICollection } from "src/interfaces/collection";
+import { ImageURL } from "../../src/entities/url";
+import { ICollection } from "../../src/interfaces/collection";
 
-export function collection(initial?: Partial<ICollection>):ICollection {
+export function collection(initial?: Partial<ICollection>): ICollection {
   return {
     title: faker.word.words(2),
     author: faker.person.fullName(),
     subtitle: faker.word.words(7),
+    image: new ImageURL(faker.image.url()),
     ...initial,
   }
 }
