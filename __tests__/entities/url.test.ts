e45809a385e52575fcd3f605a503ec55f7d2f637
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-06 11:31:06 -0500

    adiciona teste a entirade url

--- __tests__/entities/url.test.ts
+++ __tests__/entities/url.test.ts
@@ -6,13 +6,19 @@ describe('entities/url', () => {
     it('Deve retornar true quando uma URL válida for enviada', () => {
       const url = faker.image.urlPlaceholder();
 
-      expect(new ImageURL(url).isValid()).toBe(true);
+      expect(new ImageURL(url).isValid).toBe(true);
     });
 
     it('Deve retornar false quando uma URL inválida for enviada', () => {
       const url = faker.person.fullName();
 
-      expect(new ImageURL(url).isValid()).toBe(false);
-    })
+      expect(new ImageURL(url).isValid).toBe(false);
+    });
+
+    it('Deve retornar false quando uma URL for criada com uma string vazia', () => {
+      const url = '';
+
+      expect(new ImageURL(url).isValid).toBe(false);
+    });
   });
 });
