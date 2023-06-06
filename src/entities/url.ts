612718d58effe5fc054f6aa7ba19827cc6731f68
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-06 11:10:04 -0500

    valida a URL na entidade ImageURL

--- src/entities/url.ts
+++ src/entities/url.ts
@@ -1,11 +1,15 @@
+const DEFAULT_IMAGE_URL = 'https://i.ibb.co/ZHDSnj4/foo.jpg';
+
 export class ImageURL {
-  private url = '';
+  public url = DEFAULT_IMAGE_URL;
+  public isValid = false;
 
   constructor (url: string) {
     this.url = url;
+    this.isValid = this.isValidUrl()
   }
 
-  public isValid() {
+  private isValidUrl(){
     try {
       const newUrl = new URL(this.url);
       return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
