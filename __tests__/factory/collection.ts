e0c36facdbe374e8829a63a11cc462c978201e6b
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-06 16:25:40 -0500

    usa a imagem default na factory collection

--- __tests__/factory/collection.ts
+++ __tests__/factory/collection.ts
@@ -3,11 +3,13 @@ import { ImageURL } from "../../src/entities/url";
 import { ICollection } from "../../src/interfaces/collection";
 
 export function collection(initial?: Partial<ICollection>): ICollection {
+const DEFAULT_IMAGE_URL = 'https://i.ibb.co/ZHDSnj4/foo.jpg';
+
   return {
     title: faker.word.words(2),
     author: faker.person.fullName(),
     subtitle: faker.word.words(7),
-    image: new ImageURL(faker.image.url()),
+    image: new ImageURL(DEFAULT_IMAGE_URL),
     ...initial,
   }
 }
