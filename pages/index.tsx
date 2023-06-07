68acbd731dcd90041639bb48820467faf4dcc5b0
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-07 00:35:55 -0500

    usa o servico de colecao na pagina de colecoes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import { collectionService } from '../__tests__/factory/collectionService';
+
 import CollectionContainer from '../src/containers/collection'
+import { CollectionService } from '../src/service/collection';
+import api from '../src/service/api';
 
 function Collection() {
+  const collectionService = new CollectionService(api);
   return (
-    <CollectionContainer collectionService={collectionService()} />
+    <CollectionContainer collectionService={collectionService} />
   );
 }
 
