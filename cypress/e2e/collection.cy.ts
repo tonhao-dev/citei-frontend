7f1241945e7a95d4b451e79ed23eb94e3ce70db0
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-07 11:24:51 -0500

    corrige os testes e2e da pagina de colecao

--- cypress/e2e/collection.cy.ts
+++ cypress/e2e/collection.cy.ts
@@ -9,8 +9,8 @@ import {collection} from '../../__tests__/factory/collection'
 // Cypress E2E Test
 describe('e2e/collection', () => {
   describe('Deve carregar a página de coleções corretamente', () => {
-    it('Deve exibir o título da página de citações quando ela for carregada', () => {
-      cy.visit('http://localhost:3000/colecao');
+    it('Deve exibir o botão de adicionar coleção quando ela for carregada', () => {
+      cy.visit('http://localhost:8080');
 
       cy.get('button').contains('Adicionar coleção').should('be.visible');
     });
@@ -20,16 +20,18 @@ describe('e2e/collection', () => {
     it('Deve exibir a coleção que foi adicionada pelo usuário através do modal de adicionar coleção', () => {
       const newCollection = collection();
       const src = faker.image.url();
-      cy.visit('http://localhost:3000/colecao');
+      cy.visit('http://localhost:8080');
       cy.get('button').contains('Adicionar coleção').click();
 
       cy.get('input[placeholder="Título da coleção"]').type(newCollection.title);
       cy.get('input[placeholder="Subtítulo da coleção"]').type(newCollection.subtitle);
-      cy.get('input[placeholder="Autor da coleção"]').type(newCollection.author);
       cy.get('input[placeholder="Link para imagem de capa"]').type(src);
+      cy.get('input[placeholder="Autor da coleção"]').type(newCollection.author);
       cy.get('button').contains('Salvar').click();
+      cy.get('footer').contains('Rodapé').scrollIntoView();
+      cy.wait(1000);
 
-      cy.get(newCollection.title).should('be.visible');
+      cy.get('h3').contains(newCollection.title).should('be.visible');
     });
    });
 })
