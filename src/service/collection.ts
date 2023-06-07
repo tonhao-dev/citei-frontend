53e510ba33422100a43a5f51c84a1bffa2404bc1
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-06 22:13:35 -0500

    adiciona o atributo filterCollections ao servico de colecao

--- src/service/collection.ts
+++ src/service/collection.ts
@@ -2,4 +2,5 @@ import { ICollection } from "../../src/interfaces/collection";
 
 export interface ICollectionService {
   getValidCollections: () => Promise<ICollection[]>
+  filterCollections: (text: string, collections: ICollection[]) => ICollection[]
 }
