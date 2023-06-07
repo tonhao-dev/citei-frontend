acebc2dd9946b265ee601b014b48d99488a409eb
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-06 23:43:20 -0500

    move o Head para _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
 import { Cormorant } from 'next/font/google';
+import Head from 'next/head';
 
 const cormorant = Cormorant({
   weight: ['300', '400', '500', '600', '700'],
@@ -19,6 +20,10 @@ const FontFamilyVariables = () => (
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Citei - Torne-se um dev by the book</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <FontFamilyVariables />
       <Component {...pageProps} />
     </>
