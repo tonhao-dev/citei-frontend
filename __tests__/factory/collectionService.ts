c27d313b7c647a37e4a1c27378edcea3b3c6253f
Luis Antonio <luis.developer.ac@gmail.com>
2023-06-07 15:16:01 -0500

    corrige o contrato na factory de servico de colecao

--- __tests__/factory/collectionService.ts
+++ __tests__/factory/collectionService.ts
@@ -1,11 +1,12 @@
 import { ICollectionService } from "../../src/service/collection";
+import { collection } from "./collection";
 
 export function collectionService(initial?: Partial<ICollectionService>): ICollectionService {
   return {
     getValidCollections: () => new Promise(() => []),
     filterCollections: (_, collections) => collections,
     isValidCollection: (_) => true,
-    saveCollection: (_) => {},
+    saveCollection: (_) => new Promise(() => collection()),
     ...initial,
   }
 }
