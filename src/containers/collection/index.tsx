9f6ceb5106c304eb896df9e5e790274518239f70
Luis Santiago <luis.santiago@btcambio.com.br>
2024-06-28 13:01:16 -0500

    style(containers): remove footer from collection

--- src/containers/collection/index.tsx
+++ src/containers/collection/index.tsx
@@ -62,8 +62,6 @@ function Collection({ collectionService }: ICollectionProps) {
       </Modal>
 
       <Button title="Adicionar coleção" onClick={() => setModalIsOpen(!modalIsOpen)} aria-label='Adicionar coleção' />
-
-      <footer className={styles.footer}>Rodapé</footer>
     </div>
   )
 }
